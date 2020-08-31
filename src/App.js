9a7d0f07538dc2bd5dea13646225773ccacf7239
John Flanigan <johnrflanigan@gmail.com>
2020-08-31 13:54:17 -0400

    Enable toggle between pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
 import Drawer from '@material-ui/core/Drawer';
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import {makeStyles} from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -15,11 +13,13 @@ import Toolbar from "@material-ui/core/Toolbar";
 import AddIcon from '@material-ui/icons/Add';
 import TableChartIcon from '@material-ui/icons/TableChart';
 import AddSequence from "./AddSequence";
-
+import Sequences from "./Sequences";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from 'prop-types';
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
   root: {
     display: 'flex',
   },
@@ -41,61 +41,87 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3),
   },
-}));
+});
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      addSequence: true,
+      sequences: false,
+    }
+  }
+
+  clickAddSequence = () => {
+    this.setState(() => ({
+      addSequence: true,
+      sequences: false,
+    }));
+  };
+
+  clickSequences = () => {
+    this.setState(() => ({
+      addSequence: false,
+      sequences: true,
+    }));
+  };
+
+  render() {
+    const classes = this.props.classes;
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline/>
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              DNA Sequence Tool
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          anchor="left"
+        >
+          <div className={classes.toolbar}/>
+          <Divider/>
+          <List>
+            <ListItem
+              button
+              key='Add DNA Sequence'
+              onClick={this.clickAddSequence}
+              selected={this.state.addSequence}
+            >
+              <ListItemIcon><AddIcon/></ListItemIcon>
+              <ListItemText primary='Add DNA Sequence'/>
+            </ListItem>
+            <ListItem
+              button
+              key='Sequences'
+              onClick={this.clickSequences}
+              selected={this.state.sequences}
+            >
+              <ListItemIcon><TableChartIcon/></ListItemIcon>
+              <ListItemText primary='Sequences'/>
+            </ListItem>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar}/>
+          {this.state.addSequence && <AddSequence/>}
+          {this.state.sequences && <Sequences/>}
+        </main>
+      </div>
+    );
+  }
 }
 
-function App() {
-  const classes = useStyles();
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline/>
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            DNA Sequence Tool
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar}/>
-        <Divider/>
-        <List>
-          <ListItem button key='Add DNA Sequence'>
-            <ListItemIcon><AddIcon/></ListItemIcon>
-            <ListItemText primary='Add DNA Sequence'/>
-          </ListItem>
-          <ListItem button key='Sequences' selected={true}>
-            <ListItemIcon><TableChartIcon/></ListItemIcon>
-            <ListItemText primary='Sequences'/>
-          </ListItem>
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar}/>
-        <AddSequence/>
-      </main>
-    </div>
-  );
-}
-
-export default App;
+export default withStyles(styles)(App);
---
+++ src/Sequences.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+import Table from "@material-ui/core/Table";
+
+class Sequences extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return <Table/>
+  }
+}
+
+export default Sequences;
